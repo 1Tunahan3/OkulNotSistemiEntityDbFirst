b4024556024f209460555c3cd0c1f0d1812fda45
TUNAHAN KAYA <tunahankaya1@hotmail.com>
2021-01-25 18:49:39 +0300

    Add project files.

---
+++ YoutubeEntityDbFirst/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿
+namespace YoutubeEntityDbFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOgrenciListele = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnBul = new System.Windows.Forms.Button();
+            this.btnDersListesi = new System.Windows.Forms.Button();
+            this.btnNotListeleme = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFotograf = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOgrenciSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOgrenciAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btnProcedure = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(998, 195);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnOgrenciListele
+            // 
+            this.btnOgrenciListele.Location = new System.Drawing.Point(34, 258);
+            this.btnOgrenciListele.Name = "btnOgrenciListele";
+            this.btnOgrenciListele.Size = new System.Drawing.Size(130, 30);
+            this.btnOgrenciListele.TabIndex = 1;
+            this.btnOgrenciListele.Text = "Öğrenci Listele";
+            this.btnOgrenciListele.UseVisualStyleBackColor = true;
+            this.btnOgrenciListele.Click += new System.EventHandler(this.btnOgrenciListele_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(34, 294);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(130, 30);
+            this.btnKaydet.TabIndex = 2;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(34, 330);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(130, 30);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.btnGuncelle.Location = new System.Drawing.Point(34, 366);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(130, 30);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.Text = "Güncelleme";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnBul
+            // 
+            this.btnBul.Location = new System.Drawing.Point(34, 402);
+            this.btnBul.Name = "btnBul";
+            this.btnBul.Size = new System.Drawing.Size(130, 30);
+            this.btnBul.TabIndex = 5;
+            this.btnBul.Text = "Bul";
+            this.btnBul.UseVisualStyleBackColor = true;
+            this.btnBul.Click += new System.EventHandler(this.btnBul_Click);
+            // 
+            // btnDersListesi
+            // 
+            this.btnDersListesi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDersListesi.Location = new System.Drawing.Point(34, 438);
+            this.btnDersListesi.Name = "btnDersListesi";
+            this.btnDersListesi.Size = new System.Drawing.Size(130, 30);
+            this.btnDersListesi.TabIndex = 6;
+            this.btnDersListesi.Text = "Ders Listesi";
+            this.btnDersListesi.UseVisualStyleBackColor = true;
+            this.btnDersListesi.Click += new System.EventHandler(this.btnDersListesi_Click);
+            // 
+            // btnNotListeleme
+            // 
+            this.btnNotListeleme.Location = new System.Drawing.Point(34, 474);
+            this.btnNotListeleme.Name = "btnNotListeleme";
+            this.btnNotListeleme.Size = new System.Drawing.Size(130, 30);
+            this.btnNotListeleme.TabIndex = 7;
+            this.btnNotListeleme.Text = "Not Listeleme";
+            this.btnNotListeleme.UseVisualStyleBackColor = true;
+            this.btnNotListeleme.Click += new System.EventHandler(this.btnNotListeleme_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtFotograf);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtOgrenciSoyad);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtOgrenciAd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Location = new System.Drawing.Point(182, 260);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 172);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "FOTOGRAF";
+            // 
+            // txtFotograf
+            // 
+            this.txtFotograf.Location = new System.Drawing.Point(125, 137);
+            this.txtFotograf.Name = "txtFotograf";
+            this.txtFotograf.Size = new System.Drawing.Size(142, 22);
+            this.txtFotograf.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-3, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "OGRENCI SOYAD";
+            // 
+            // txtOgrenciSoyad
+            // 
+            this.txtOgrenciSoyad.Location = new System.Drawing.Point(125, 103);
+            this.txtOgrenciSoyad.Name = "txtOgrenciSoyad";
+            this.txtOgrenciSoyad.Size = new System.Drawing.Size(142, 22);
+            this.txtOgrenciSoyad.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "OGRENCI AD";
+            // 
+            // txtOgrenciAd
+            // 
+            this.txtOgrenciAd.Location = new System.Drawing.Point(125, 72);
+            this.txtOgrenciAd.Name = "txtOgrenciAd";
+            this.txtOgrenciAd.Size = new System.Drawing.Size(142, 22);
+            this.txtOgrenciAd.TabIndex = 2;
+            this.txtOgrenciAd.TextChanged += new System.EventHandler(this.txtOgrenciAd_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(125, 33);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(142, 22);
+            this.txtId.TabIndex = 0;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(763, 358);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(123, 33);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Linq Entity";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btnProcedure
+            // 
+            this.btnProcedure.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProcedure.Location = new System.Drawing.Point(34, 510);
+            this.btnProcedure.Name = "btnProcedure";
+            this.btnProcedure.Size = new System.Drawing.Size(130, 30);
+            this.btnProcedure.TabIndex = 11;
+            this.btnProcedure.Text = "Prosedür";
+            this.btnProcedure.UseVisualStyleBackColor = true;
+            this.btnProcedure.Click += new System.EventHandler(this.btnProcedure_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(527, 416);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(152, 21);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ada göre sırala A-Z";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(527, 330);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(80, 21);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "İlk 3 Kişi";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(527, 389);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(118, 21);
+            this.radioButton4.TabIndex = 15;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "A ile Başlayan";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(527, 362);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(92, 21);
+            this.radioButton5.TabIndex = 16;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "E ile Biten";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(527, 299);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(171, 21);
+            this.radioButton6.TabIndex = 17;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Sınav 1 Ortalama Üstü";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(527, 267);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(140, 21);
+            this.radioButton7.TabIndex = 18;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Sınav 1 En İyi Not";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(527, 447);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(152, 21);
+            this.radioButton2.TabIndex = 19;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ada göre sırala Z-A";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 590);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btnProcedure);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnNotListeleme);
+            this.Controls.Add(this.btnDersListesi);
+            this.Controls.Add(this.btnBul);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnOgrenciListele);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOgrenciListele;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnBul;
+        private System.Windows.Forms.Button btnDersListesi;
+        private System.Windows.Forms.Button btnNotListeleme;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFotograf;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOgrenciSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOgrenciAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btnProcedure;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ YoutubeEntityDbFirst/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace YoutubeEntityDbFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DbSınavOgrenciEntities entities = new DbSınavOgrenciEntities();
+
+        private void btnDersListesi_Click(object sender, EventArgs e)
+        {
+            DbSınavOgrenciEntities entities=new DbSınavOgrenciEntities();
+            dataGridView1.DataSource = entities.TBLDERSLER.ToList();
+        }
+
+        private void btnOgrenciListele_Click(object sender, EventArgs e)
+        {
+           
+            dataGridView1.DataSource = entities.TBLOGRENCI.ToList();
+            dataGridView1.Columns[3].Visible = false;
+            dataGridView1.Columns[4].Visible = false;
+        }
+
+        private void btnNotListeleme_Click(object sender, EventArgs e)
+        {
+            var query = from item in entities.TBLNOTLAR
+                select new
+                {
+                    item.NOTID, item.OGR, item.DERS, item.SINAV1, item.SINAV2, item.SINAV3, item.ORTALAMA, item.DURUM
+                };
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            TBLOGRENCI t=new TBLOGRENCI();
+            t.AD = txtOgrenciAd.Text;
+            t.SOYAD = txtOgrenciSoyad.Text;
+            entities.TBLOGRENCI.Add(t);
+            entities.SaveChanges();
+            MessageBox.Show("Eklendi");
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var x = entities.TBLOGRENCI.Find(id);
+            entities.TBLOGRENCI.Remove(x);
+            entities.SaveChanges();
+            MessageBox.Show("Silindi");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var x = entities.TBLOGRENCI.Find(id);
+            x.AD = txtOgrenciAd.Text;
+            x.SOYAD = txtOgrenciSoyad.Text;
+            x.FOTOGRAF = txtFotograf.Text;
+            entities.SaveChanges();
+            MessageBox.Show("Güncellendi");
+        }
+
+        private void btnProcedure_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = entities.NOTLISTESI();
+        }
+
+        private void btnBul_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource=entities.TBLOGRENCI.Where(x=>x.AD==txtOgrenciAd.Text | x.SOYAD==txtOgrenciSoyad.Text).ToList();
+        }
+
+        private void txtOgrenciAd_TextChanged(object sender, EventArgs e)
+        {
+            string aranan = txtOgrenciAd.Text;
+            var degerler = from item in entities.TBLOGRENCI
+                where item.AD.Contains(aranan)
+                select item;
+            dataGridView1.DataSource = degerler.ToList();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                List<TBLOGRENCI> list = entities.TBLOGRENCI.OrderBy(p => p.AD).ToList();
+                dataGridView1.DataSource = list;
+            }
+
+            if (radioButton2.Checked == true)
+            {
+                List<TBLOGRENCI> list2 = entities.TBLOGRENCI.OrderByDescending(p => p.AD).ToList();
+                dataGridView1.DataSource = list2;
+            }
+
+            if (radioButton3.Checked == true)
+            {
+                List<TBLOGRENCI> list3 = entities.TBLOGRENCI.OrderBy(p => p.AD).Take(3).ToList();
+                dataGridView1.DataSource = list3;
+            }
+
+            if (radioButton4.Checked==true)
+            {
+                List<TBLOGRENCI> list = entities.TBLOGRENCI.Where(p => p.AD.StartsWith("a")).ToList();
+                dataGridView1.DataSource = list;
+            }
+
+            if (radioButton5.Checked==true)
+            {
+                List<TBLOGRENCI> list = entities.TBLOGRENCI.Where(p => p.AD.EndsWith("e")).ToList();
+                dataGridView1.DataSource = list;
+            }
+
+            if (radioButton6.Checked==true)
+            {
+                var ortalama = entities.TBLNOTLAR.Average(p => p.SINAV1);
+                List<TBLNOTLAR> list12 = entities.TBLNOTLAR.Where(p=>p.SINAV1 > ortalama).ToList();
+                dataGridView1.DataSource = list12;
+            }
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            //var top = entities.TBLNOTLAR.Max(p => p.SINAV1);
+            //List<TBLNOTLAR> list13 = entities.TBLNOTLAR.Where(p => p.SINAV1 == top).ToList();
+            //var list14 = entities.TBLOGRENCI.Where(p => p.ID = list13).ToList();
+            //dataGridView1.DataSource = list13;
+        }
+    }
+}
